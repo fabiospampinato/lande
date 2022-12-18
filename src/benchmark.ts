3431906a64f8a84becb3527d59ce50d01ab8d587
Fabio Spampinato <spampinabio@gmail.com>
2022-12-18 02:53:13 +0100

    Added a benchmark script

---
+++ src/benchmark.ts
@@ -0,0 +1,91 @@
+
+/* IMPORT */
+
+import {loadModule} from 'cld3-asm';
+import {franc as franc} from 'franc';
+import {franc as francAll} from 'franc-all';
+import {franc as francMin} from 'franc-min';
+import fs from 'node:fs';
+import lande from '../standalone/t50.js';
+import {DATASET_PATH, DATASET_BENCHMARK_LIMIT} from './constants';
+import {CONFIGS} from './constants';
+import {forEachLine} from './utils';
+import type {DatasetTest, DatumTest} from './types';
+
+/* HELPERS */
+
+const getDataset = ( langs: string[] ): DatasetTest => {
+
+  const dataset: DatasetTest = {};
+  const langsSet = new Set ( langs );
+  const csv = fs.readFileSync ( DATASET_PATH );
+
+  forEachLine ( csv, line => {
+
+    const parts = line.split ( '\t' );
+
+    if ( parts.length !== 3 ) return; // Something went wrong with this line
+
+    const lang = parts[1];
+    const sentence = parts[2];
+
+    if ( !langsSet.has ( lang ) ) return;
+
+    if ( dataset[lang]?.length >= DATASET_BENCHMARK_LIMIT ) return; // Already parsed enough sentences
+
+    const datum: DatumTest = { lang, sentence };
+
+    dataset[lang] ||= [];
+    dataset[lang].push ( datum );
+
+  });
+
+  return dataset;
+
+};
+
+/* MAIN */
+
+const cldFactory = await loadModule ();
+const cld = cldFactory.create ();
+const dataset = getDataset ( CONFIGS[0].langs );
+
+console.time ( 'cld3' );
+Object.values ( dataset ).forEach ( data => {
+  data.forEach ( datum => {
+    cld.findLanguage ( datum.sentence );
+  });
+});
+console.timeEnd ( 'cld3' );
+
+console.time ( 'franc' );
+Object.values ( dataset ).forEach ( data => {
+  data.forEach ( datum => {
+    franc ( datum.sentence );
+  });
+});
+console.timeEnd ( 'franc' );
+
+console.time ( 'francAll' );
+Object.values ( dataset ).forEach ( data => {
+  data.forEach ( datum => {
+    francAll ( datum.sentence );
+  });
+});
+console.timeEnd ( 'francAll' );
+
+console.time ( 'francMin' );
+Object.values ( dataset ).forEach ( data => {
+  data.forEach ( datum => {
+    francMin ( datum.sentence );
+  });
+});
+console.timeEnd ( 'francMin' );
+
+console.time ( 'lande' );
+Object.values ( dataset ).forEach ( data => {
+  data.forEach ( datum => {
+    lande ( datum.sentence );
+  });
+});
+console.timeEnd ( 'lande' );
