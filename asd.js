d1eccd42f9a5fd057cf70ca699caadc28a4a3b50
Fabio Spampinato <spampinabio@gmail.com>
2022-12-21 00:31:30 +0100

    Deleted a leftover file

--- asd.js
+++
@@ -1,4 +0,0 @@
-import lande from './standalone/t50.js';
-
-console.log ( lande ( 'What language is this sentence written in?' ) ); // => [['eng', 0.9999921321868896], ['deu', 0.000002357382982154377], ['heb', 0.000001461773877053929], ...]
-console.log ( lande ( 'In che lingua è scritta questa frase?' ) ); // => [['ita', 0.9999935626983643], ['ell', 0.0000025603442281862954], ['vie', 0.000002114558583343751], ...]
